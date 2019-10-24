5f39386c85f6d06041e87107593f8864e5b2c5d7
leoli51 <46094699+leoli51@users.noreply.github.com>
2019-10-24 12:15:58 +0200

    added install require RPi

--- setup.py
+++ setup.py
@@ -16,14 +16,22 @@ class InstallCommand(install):
 
     def run(self):
         install.run(self)  # OR: install.do_egg_install(self)
+        
+        #copy script inside autostart script folder
         fan_controller_path = os.path.abspath(pkg_resources.resource_filename(package_name, "fan_controller.py"))
         final_path = os.path.join("/home/melopero-autostart/scripts/", "fan_controller.py")
         shutil.copyfile(fan_controller_path, final_path)
         os.chmod(final_path, 0o664)    
+        
+        #copy uninstall script inside autostart uninstall scripts folder
+        uninstall_script_path = os.path.abspath(pkg_resources.resource_filename(package_name, "uninstall_melopero_fan_hat.sh"))
+        final_uninstall_path = os.path.join("/home/melopero-autostart/uninstall/uninstall-scripts/", "uninstall_melopero_fan_hat.sh")
+        shutil.copyfile(uninstall_script_path, final_uninstall_path)
+        os.chmod(final_uninstall_path, 0o554)
 
 setuptools.setup(
     name="melopero_fan_hat",
-    version="0.0.1",
+    version="0.0.4",
     description="Melopero Fan Hat, automatically sets up your melopero-fan-hat",
     #url="https://github.com/melopero/Melopero_AMG8833/tree/master/module",
     author="Melopero",
@@ -31,7 +39,7 @@ setuptools.setup(
     license="MIT",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires = ['melopero_autostart>=0.0.6'],
+    install_requires = ['melopero_autostart>=0.0.7', 'RPi.GPIO'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
